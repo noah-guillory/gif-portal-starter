4881cc712290cac2b8480d5434afe13f2ed8c901
Noah Guillory <noah@noahguillory.me>
2021-12-05 10:03:13 -0600

    Make the linter happy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import kp from './keypair.json'
 import './App.css';
 
 // SystemProgram is a reference to the Solana runtime!
-const { SystemProgram, Keypair } = web3;
+const { SystemProgram } = web3;
 
 const arr = Object.values(kp._keypair.secretKey)
 const secret = new Uint8Array(arr)
@@ -28,13 +28,6 @@ const opts = {
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-
-const TEST_GIFS = [
-  'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
-  'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
-  'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
-  'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
-]
 
 const App = () => {
 
@@ -198,7 +191,7 @@ const App = () => {
             {/* We use index as the key instead, also, the src is now item.gifLink */}
             {gifList.map((item, index) => (
               <div className="gif-item" key={index}>
-                <img src={item.imageLink} />
+                <img src={item.imageLink} alt="" />
                 <button onClick={upvoteImage(item.imageLink)}>Upvote</button>
                 <sub className="submitter-text">Submitter: {item.userAddress.toString()}</sub>
                 <sub className="submitter-text">Votes {item.votes}</sub>
